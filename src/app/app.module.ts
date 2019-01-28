541de5d44fbf5950467bb7ee3f841fe1b22e12a5
Jas Nijjer <jas@minchindowsing.co.uk>
2019-01-28 12:05:10 +0000

    added bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,29 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { DemoComponent } from './demo/demo.component';
 import { DemoService } from './services/demo.service';
+import { InfoComponent } from './components/info/info.component';
+import { NavComponent } from './components/core/nav/nav.component';
+
+const appRoutes: Routes = [
+  { path: 'info', component: InfoComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    DemoComponent
+    DemoComponent,
+    InfoComponent,
+    NavComponent
   ],
   imports: [
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
     BrowserModule,
     AppRoutingModule
   ],
