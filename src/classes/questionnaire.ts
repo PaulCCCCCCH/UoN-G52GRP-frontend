2ad6ba5b2ce625415c517ce65c6a01ab6ae002f8
PaulCCCCCCH <576140292@qq.com>
2019-01-29 22:44:36 +0000

    changed qlist page

--- src/classes/questionnaire.ts
+++ src/classes/questionnaire.ts
@@ -1,10 +1,17 @@
 export class Questionnaire {
   id: number;
   title: string;
+  dateAssigned: Date;
+  deadline: Date;
+  description: string;
   otherInfo: string; // Need more (date, creator, pointers, etc.)
+
 
   constructor(id: number, title: string) {
     this.id = id;
     this.title = title;
+    this.dateAssigned = new Date();
+    this.deadline = new Date();
+    this.description = 'Something about the questionnaire';
   }
 }
