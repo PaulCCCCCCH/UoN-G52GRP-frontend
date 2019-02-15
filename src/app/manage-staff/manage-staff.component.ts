3c0f999e5bc932396ba1d616c846b6b5ae11c15a
Meng WU <psymw8@exmail.nottingham.ac.uk>
2019-02-15 15:10:56 +0000

    Improve overall design for assign and manage-staff page, change the manage-staff page because orginial bootstrap use javasricipt, cannot work on angular

--- src/app/manage-staff/manage-staff.component.ts
+++ src/app/manage-staff/manage-staff.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgbModalConfig, NgbModal } from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-manage-staff',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManageStaffComponent implements OnInit {
 
-  constructor() { }
+  constructor (config: NgbModalConfig, private modalService: NgbModal) {
+    // customize default values of modals used by this component tree
+    config.backdrop = 'static';
+    config.keyboard = false;
+    
+   }
 
   ngOnInit() {
+  }
+  open(content) {
+    this.modalService.open(content);
   }
 
 }
