e56499ca1c8199d6927c01b856215e3f1263b98d
PaulCCCCCCH <paulcccccch@gmail.com>
2019-02-15 20:29:02 +0000

    temp commit

---
+++ src/app/services/questionnaire/questionnaire.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Questionnaire } from '../../../classes/questionnaire';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class QuestionnaireService {
+
+  private qListUrl = 'https://my-json-server.typicode.com/paulcccccch/demo/questionnaires';
+
+  constructor(private httpClient: HttpClient) { }
+
+  getQList(): Observable<Questionnaire[]> {
+    return this.httpClient.get<Questionnaire[]>(this.qListUrl);
+  }
+
+  getQuestionnaire(id: number): Observable<Questionnaire> {
+    return this.httpClient.get<Questionnaire>(this.qListUrl + `/${id}`);
+  }
+}
